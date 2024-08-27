e08354b5b262152b7626f57e86b0ee190874fab8
Angelina Zhou <zhouangelina3@gmail.com>
2024-08-26 22:23:18 -0400

    Fixed bug where video starts playing only after hovering over it multiple times. Updated UI.
    Added autoplay and muted attributes to video tags to fix bug. Video now plays immediately upon hover.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const projectBoxes = document.querySelectorAll(".project-box.graphic");
 projectBoxes.forEach((box)=>{
     const video = box.querySelector("video");
+    
     box.addEventListener("mouseenter", () => {
         video.currentTime = 0;
         video.style.opacity = 1;
