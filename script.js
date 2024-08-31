d8895ff2b1ba61792fe09e81a5dcd95f7e2a45fa
Angelina Zhou <zhouangelina3@gmail.com>
2024-08-30 21:15:26 -0400

    Added dark mode.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function setTheme(){
+    const root = document.documentElement;
+    const newTheme = root.className === "light"? "dark" : "light";
+    root.className = newTheme;
+    if (root.className === "dark"){
+        darkModeIcon.src = "images/light-mode-icon.svg";
+        darkModeIcon.alt = "Dark Mode Icon";
+    }
+    else {
+        darkModeIcon.src = "images/dark-mode-icon.svg";
+        darkModeIcon.alt = "Light Mode Icon";
+    }
+}
+
+
+document.documentElement.className = 'light';
+darkModeIcon = document.querySelector(".dark-mode-icon");
+darkModeIcon.addEventListener("click", setTheme)
+
 const projectBoxes = document.querySelectorAll(".project-box.graphic");
 projectBoxes.forEach((box)=>{
     const video = box.querySelector("video");
@@ -13,3 +32,5 @@ projectBoxes.forEach((box)=>{
         video.style.opacity = 0;
     })
 })
+
+
